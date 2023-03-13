4a3c2dc5b47e815375bc320d4ec190ff70d9ce16
NabiL Ashbat <nbl.dev21@gmail.com>
2023-03-13 03:15:12 +0300

    ench: set some global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
 		'./app/**/*.{js,ts,jsx,tsx}',
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
-
-		// Or if using `src` directory:
-		'./src/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-black': '#1A232E',
+				'secondary-black': '#c7c7c7',
+			},
+		},
 	},
 	plugins: [],
 }

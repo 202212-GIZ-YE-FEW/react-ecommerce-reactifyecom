0011aa7478cf4da889b6637a599d6153d9e842fa
MariaAbdullah97 <meroo3066@gmail.com>
2023-03-15 22:55:14 +0300

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import '@/styles/globals.css'
 import '@/styles/products.css'
-
-import { Inter } from 'next/font/google'
 import Footer from './Footer'
 import Navbar from './Navbar'
+
+import { Inter } from 'next/font/google'
+
 
 const inter = Inter({
 	subsets: ['latin'],

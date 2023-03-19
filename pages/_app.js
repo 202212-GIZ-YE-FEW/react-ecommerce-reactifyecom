54e983acff487a83c2f39b7db54ec43d2575c62b
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-03-19 19:16:47 +0300

    fix: change footer position to be under the Nextjs Routing Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,24 @@
-import '@/styles/globals.css'
-import '@/styles/products.css'
+import "@/styles/globals.css";
+import "@/styles/products.css";
 // import Footer from './Footer'
 // import Navbar from './Navbar'
 
-import { Inter } from 'next/font/google'
-import Footer from './Footer'
-import Navbar from './Navbar'
-
+import { Inter } from "next/font/google";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const inter = Inter({
-	subsets: ['latin'],
-	variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }) {
-	return (
-		<main className={`${inter.variable} font-sans`}>
-			<Navbar/>
-			<Footer/>
-			<Component {...pageProps} />
-		</main>
-	)
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Navbar />
+
+      <Component {...pageProps} />
+      <Footer />
+    </main>
+  );
 }
